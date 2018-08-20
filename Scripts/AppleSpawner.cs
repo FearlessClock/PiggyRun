af3b167647e6d648a0a0af27893cce535e0a8dc7
Pieter ALGERA <pieter.algera@devinci.fr>
2018-08-20 22:47:14 +0200

    Added apples

---
+++ Scripts/AppleSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleSpawner : MonoBehaviour {
+
+    public GameStateManager GSM;
+
+    public GameObject food;
+    public float chanceToSpawnFood;
+    public float floatingHeight;
+
+    public Transform player;
+    public float inFrontOfPlayer;
+    public float aboveTheGround;
+    public LayerMask ground;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(GSM.gameState == GameState.GAMEPLAY)
+        {
+            if (Random.Range(0, 100) > chanceToSpawnFood)
+            {
+                Vector3 pos = player.position;
+                pos += Vector3.right * inFrontOfPlayer;
+                pos += Vector3.up * aboveTheGround;
+
+                RaycastHit2D isGrounded = Physics2D.Raycast(pos, Vector2.down, 30, ground);
+                pos += Vector3.down *( isGrounded.distance - floatingHeight);
+
+                GameObject apple = Instantiate<GameObject>(food, pos, Quaternion.identity);
+
+                apple.transform.parent = this.transform;
+            }
+        }
+	}
+}
---
+++ Scripts/FloatObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatObject : MonoBehaviour {
+
+    public float floatingAmount;
+    public float speed;
+    private Vector3 startPosition;
+    private float time;
+
+	// Use this for initialization
+	void Start () {
+        startPosition = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = startPosition + Vector3.up * (Mathf.Sin(time) * floatingAmount);
+        time += speed * Time.deltaTime;
+	}
+}
